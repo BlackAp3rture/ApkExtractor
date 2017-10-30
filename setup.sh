1c50821a7700649cfb1e93dc801bd858044481cb
BlackAp3rture <twhitmi@gmail.com>
2017-10-29 21:46:44 -0400

    reqs fix

--- setup.sh
+++ setup.sh
@@ -2,9 +2,13 @@
 
 # Upgrade PIP
 pip install --upgrade pip
+pip2 install --upgrade pip
 
 # Install requirements
 pip install -r requirements
+pip2 install -r requirements
+
+brew install libusb
 
 # Download and build apktool
 wget https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.2.3.jar
